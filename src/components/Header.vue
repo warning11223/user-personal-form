e0abf4c8c5bd358450ddd75e878772ca1f0a5003
warning11223 <warning11223@gmail.com>
2025-05-28 16:48:34 +0300

    fix: стили

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,24 @@
 <template>
   <header class="header">
-    <RouterLink to="/">
-      <Logo />
-    </RouterLink>
-    <div class="link-wrapper">
-      <RouterLink
-        :class="['link', { active: route.path === '/' }]"
-        to="/">
-        Форма
-      </RouterLink>
+   <div class="wrapper">
+     <RouterLink to="/">
+       <Logo />
+     </RouterLink>
+     <div class="link-wrapper">
+       <RouterLink
+         :class="['link', { active: route.path === '/' }]"
+         to="/">
+         Форма
+       </RouterLink>
 
-      <RouterLink
-        :class="['link', { active: route.path === '/preview' }]"
-        to="/preview">
-        Превью
-      </RouterLink>
-    </div>
-    <div class="block"></div>
+       <RouterLink
+         :class="['link', { active: route.path === '/preview' }]"
+         to="/preview">
+         Превью
+       </RouterLink>
+     </div>
+     <div class="block"></div>
+   </div>
   </header>
 </template>
 
@@ -29,36 +31,41 @@ const route = useRoute()
 
 <style lang="scss" scoped>
 .header {
-  height: 76px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 92px;
   box-shadow: 0px 1px 0px 0px rgba(17, 17, 17, 0.1);
+  padding: 0 92px;
 
-  .block {
-    width: 175px;
-  }
-
-  .link-wrapper {
+  .wrapper {
+    height: 76px;
     display: flex;
     align-items: center;
-    gap: 24px;
+    justify-content: space-between;
+    max-width: 1366px;
+    margin: 0 auto;
 
-    .link {
-      color: rgba(17, 17, 17, 0.48);
-      font-size: 14px;
-      font-weight: 400;
-      text-decoration: none;
-      border-bottom: 2px solid transparent;
-      padding-bottom: 5px;
+    .block {
+      width: 175px;
+    }
 
-      &:hover {
-        border-bottom: 2px solid rgba(17, 17, 17, 0.48);
-      }
+    .link-wrapper {
+      display: flex;
+      align-items: center;
+      gap: 24px;
 
-      &.active {
-        border-bottom: 2px solid rgb(1, 167, 253);
+      .link {
+        color: rgba(17, 17, 17, 0.48);
+        font-size: 14px;
+        font-weight: 400;
+        text-decoration: none;
+        border-bottom: 2px solid transparent;
+        padding-bottom: 5px;
+
+        &:hover {
+          border-bottom: 2px solid rgba(17, 17, 17, 0.48);
+        }
+
+        &.active {
+          border-bottom: 2px solid rgb(1, 167, 253);
+        }
       }
     }
   }
